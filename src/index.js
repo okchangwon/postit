910a1dbc80f6524f7c79ba0144d5645ab9063d39
changwon.ok <changwon.ok@nhn.com>
2018-04-11 13:45:23 +0900

    jQuery 세팅, 번들 분할

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 document.write("Develop Postit!");
+console.log($);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public/js'),
-    filename: 'postit.bundle.js'
+    filename: 'postit.[name].bundle.js',
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
