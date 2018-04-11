496efa4ee5d586be1043f995c990a663d8ee243d
changwon.ok <changwon.ok@nhn.com>
2018-04-11 13:29:44 +0900

    개발 환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'postit.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "/js",
+    port: 9000,
+    open: true
+  }
+};
