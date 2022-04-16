a64f3f985d90f9038bbfc0ccf5c1ddbff50c03db
ok-changwon <ok.changwon@navercorp.com>
2022-04-16 11:28:44 +0900

    chore: index 경로 수정

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"),
+      template: path.resolve(__dirname, "src/index.html"),
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
